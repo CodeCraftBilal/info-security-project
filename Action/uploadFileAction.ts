7681cad95319f6230ad9479c02ae79161adf45f2
bilalkhan <bilalkhan751150@gmail.com>
2025-06-14 11:11:02 +0500

    files uploaded and database work properly

--- Action/uploadFileAction.ts
+++ Action/uploadFileAction.ts
@@ -5,6 +5,11 @@ import { v2 as cloudinary } from "cloudinary";
 export async function uploadFileAction(formData: FormData) {
     const files = formData.getAll('files');
     const keys = formData.getAll('encryptedKey');
+    const ivs = formData.getAll('iv')
+
+    console.log('files are: ', files)
+    console.log('keys are: ', keys)
+    console.log('ivs: ' , ivs)
 
     cloudinary.config({
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -19,12 +24,21 @@ export async function uploadFileAction(formData: FormData) {
     let uploadedUrls: string[] = [];
 
     try {
+        let i = 0;
         for (const file of files) {
             if (file instanceof Blob) {
                 const arrayBuffer = await file.arrayBuffer();
                 const buffer = Buffer.from(arrayBuffer); // Convert to Node.js Buffer
                 console.log('Uploading buffer:', buffer.length, buffer.constructor.name);
-                
+
+                // Read key and iv as Buffers
+                const keyFile = keys[i] as File;
+                const keyBuffer = Buffer.from(await keyFile.arrayBuffer());
+
+                const ivFile = ivs[i] as File;
+                const ivBuffer = Buffer.from(await ivFile.arrayBuffer());
+
+                // start uploading to cloudinary
                 const uploadResult: any = await new Promise((resolve, reject) => {
                     const uploadStream = cloudinary.uploader.upload_stream(
                         { folder: 'encryptedFiles', resource_type: 'raw' },
@@ -37,10 +51,28 @@ export async function uploadFileAction(formData: FormData) {
                 });
 
                 uploadedUrls.push(uploadResult.secure_url);
+
+                // store data in the mongodb
+                const client = await clientPromise;
+                const db = client.db('secureShare')
+
+                await db.collection('files').insertOne({
+                    fileName: file.name,
+                    cloudinaryUrl: uploadResult.secure_url,
+                    encryptedAesKey: keyBuffer.toString('base64'),
+                    mimeType: file.type,
+                    iv: ivBuffer.toString('base64'),
+                    uploaderId: 'userId',
+                    uploadDate: new Date()
+                })
             }
+            i+=1
         }
         console.log('urls : ', uploadedUrls)
+
+
         return { message: 'Upload successful', urls: uploadedUrls, error: false };
+
 
     } catch (error) {
         console.error('Upload failed:', error);
--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -5,14 +5,15 @@ import { uploadFileAction } from '@/Action/uploadFileAction';
 import Link from 'next/link';
 import { CryptoService, KeyPair, EncryptedFile } from '@/lib/crypto';
 import { blob } from 'stream/consumers';
+import MyFiles from '@/components/MyFiles'
 
 const Dashboard = (): React.JSX.Element => {
 
   interface EncryptedFileWithMetaData extends EncryptedFile {
     fileName: string,
-    fileType: string,
-    encryptedKey: ArrayBuffer
+    fileType: string
   }
+    // encryptedKey: ArrayBuffer
 
   type FileMetaData = {
     id: number,
@@ -301,6 +302,10 @@ const Dashboard = (): React.JSX.Element => {
 
     // Optional: add metadata if needed
     formData.append('encryptedKey', new Blob([file.encryptedKey]), file.fileName + '.key');
+
+    console.log(file.iv)
+    formData.append('iv', new Blob([file.iv]), file.fileName + '.iv');
+  
   }
 
   const r = await uploadFileAction(formData);
@@ -372,62 +377,7 @@ const Dashboard = (): React.JSX.Element => {
 
         {/* Right Section */}
         <div className="right flex flex-col items-center gap-2 bg-blue-200 w-[70%] h-full rounded-2xl px-2">
-          <div className="search flex items-center gap-2 rounded-lg mt-4 p-1 bg-blue-300 mx-2 w-[90%]">
-            <input onChange={(e) => handleChangeSearch(e)} type="search" className='w-[calc(100%-60px)] py-1 px-2 text-black out text-xl focus:outline-hidden' />
-            <button><img className='' src="/search.png" width={35} height={35} alt="search" /></button>
-          </div>
-          {/* flex flex-wrap gap-3 w-full h-[80%] overflow-auto */}
-          <div className="filescontainer flex flex-wrap gap-3 min-h-0 max-h-full overflow-auto">
-            {filteredFiles.map((file) => {
-              return (
-                <div
-                  key={file.id}
-                  ref={(el) => {
-                    menuRefs.current[file.id] = el
-                  }}
-                  className={`${openMenuId === file.id ? 'border-gray-300 border-2 shadow-xl' : ''} relative min-w-[250px] max-h-[76px] flex flex-grow items-center gap-2 text-black bg-white py-2 px-2 rounded-xl max-w-[48%]`}
-                >
-                  <div className="icon rounded-full">
-                    <img className='rounded-full' src="/File.jpg" alt="file icon" width={60} height={60} />
-                  </div>
-                  <div className="data w-[calc(100%-65px)]">
-                    <div className="filename text-lg font-bold"><span>{file.name}</span></div>
-                    <div className='other flex gap-2 w-full justify-between pr-3'>
-                      <span className="size">{file.size}</span>
-                      <span className="date">{file.uploadedAt}</span>
-                      <span className="type">{file.type}</span>
-                    </div>
-                  </div>
-                  <div className="menu absolute right-4 top-2">
-                    <img
-                      className='cursor-pointer'
-                      onClick={() =>
-                        setOpenMenuId(openMenuId === file.id ? null : file.id)
-                      }
-                      src="/menu_dots.png"
-                      alt="menu"
-                      width={20}
-                      height={30}
-                    />
-                    <ul
-                      className={`${openMenuId === file.id ? '' : 'hidden'
-                        } rounded-sm p-2 bg-gray-400 absolute right-0 z-10`}
-                    >
-                      <li className='cursor-pointer hover:bg-gray-500 p-1 rounded-sm'>View</li>
-                      <li className='cursor-pointer hover:bg-gray-500 p-1 rounded-sm'>Delete</li>
-                      <li className='cursor-pointer hover:bg-gray-500 p-1 rounded-sm'>Download</li>
-                    </ul>
-                  </div>
-                </div>
-              );
-            })}
-            {filteredFiles.length === 0 && (
-              <div>
-                No fileFound matching your search
-              </div>
-            )}
-
-          </div>
+          <MyFiles />
         </div>
       </div>
     </div>
